37681ca80c921dd1c0aa21995beba0bf3759f9d9
dario-loi <77234595+dario-loi@users.noreply.github.com>
2025-10-22 12:34:59 +0200

    Esercizio filtro
    Fatto l'esercizio filtro degli esercizi 4.

---
+++ filtra.py
@@ -0,0 +1,24 @@
+
+lista = [1,2,3,4,5,6,7]
+insieme = set([1,2,3])
+
+lista_nuova = []
+
+for n in lista:
+    if n in insieme:
+        continue
+    lista_nuova.append(n)
+    
+print(lista_nuova)
+
+
+## Usando Filter e Map
+
+print(
+    list(
+        filter(
+            lambda x : x not in insieme,
+            lista
+        )
+    )
+)

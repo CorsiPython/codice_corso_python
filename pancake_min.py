65c8675925b11dbc708318b8423926e16583b283
dario-loi <77234595+dario-loi@users.noreply.github.com>
2025-10-24 10:08:17 +0200

    Fix min pancake formula

--- pancake_min.py
+++ pancake_min.py
@@ -5,7 +5,7 @@ def bake_pancakes(latte, farina):
     pancakes_milk = latte // 250
     pancakes_flour = farina // 200
         
-    return min(pancakes_milk, pancakes_flour)
+    return min(pancakes_milk, pancakes_flour) * 4
 
 
 # === programma principale

442a732113e567783c1749a860cd142c84317ad7
dario-loi <77234595+dario-loi@users.noreply.github.com>
2025-10-24 11:52:25 +0200

    File exercises pt.2

---
+++ conta_righe_vocali.py
@@ -0,0 +1,16 @@
+# Dato il file lines.txt , scrivi un programma che legge il file e conta quante righe
+# contengono almeno una vocale (a, e, i, o, u, sia maiuscole che minuscole). Stampa il
+# conteggio totale delle righe con almeno una vocale.
+
+conto = 0
+vocali = list("aeiou")
+
+
+with open("lines.txt", 'r') as file:
+    for line in file:
+        for vocale in vocali:
+            if vocale in line.lower():
+                conto += 1
+                break
+            
+print(f"Il numero di righe con almeno una vocale è: {conto}")
---
+++ elimina_vocali.py
@@ -0,0 +1,39 @@
+# Dato il file lines.txt , scrivi un programma che legge il file e crea un nuovo file chiamato
+# no_vowels.txt in cui tutte le vocali (a, e, i, o, u, sia maiuscole che minuscole) sono state
+# rimosse da ogni riga.
+
+def filtra_lista(contenuto):
+    
+    filtrato = [
+        lettera for lettera in contenuto 
+        if lettera.lower() not in "aeiou"
+    ]
+    return "".join(filtrato)
+
+def filtra_lista_for(contenuto):
+    
+    filtrato = []
+    for lettera in contenuto:
+        if lettera.lower() not in "aeiou":
+            filtrato.append(lettera)
+            
+    return "".join(filtrato)
+
+def filtra_mapfilter(contenuto):
+    
+    return "".join(
+        list(
+            filter(
+                lambda lettera : lettera.lower() not in "aeiou",
+                contenuto
+            )
+        )
+    )
+
+with open('lines.txt', 'r') as file:
+    content = file.read()
+    
+contenuto_filtrato = filtra_mapfilter(content)
+
+with open("no_vowels.txt", 'w') as f:
+    f.write(contenuto_filtrato)
